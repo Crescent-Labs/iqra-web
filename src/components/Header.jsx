6e6a1cf85e75e62726732696c96d18e14bbdf6ea
Baraa Hamodi <bhamodi@uwaterloo.ca>
2016-10-10 01:18:41 -0700

    Implemented basic navbar header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const Header = () => (
-    <div className="header">
-        <a href="/" className="logo-container">
-            <img src="/static/img/logo.png" className="logo" alt="logo" />
-        </a>
-    </div>
+    <nav>
+        <div className="header">
+            <a href="/" className="logo-container">
+                <img src="/static/img/logo.png" className="logo" alt="logo" />
+            </a>
+            <a href="#" data-activates="mobile" className="dropdown button-collapse">
+                <i className="material-icons">menu</i>
+                <ul className="mobile-nav" id="mobile">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/app">Search</a></li>
+                </ul>
+            </a>
+            <ul className="desktop-nav hide-on-med-and-down">
+                <li><a href="/">Home</a></li>
+                <li><a href="/app">Search</a></li>
+            </ul>
+        </div>
+    </nav>
 );
 
 export default Header;
